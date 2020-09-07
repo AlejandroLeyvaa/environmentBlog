5f0bfbcee7cb560e4967d807695d70d803cfca0f
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2020-09-07 14:44:59 -0600

    Post schema created

---
+++ src/utils/schemas/posts.js
@@ -0,0 +1,27 @@
+const joi = require('joi');
+
+const postIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const postTitleSchema = joi.string().max(80);
+const postContent = joi.string().max(6000);
+const postImage = joi.string().uri();
+const postAuthor = joi.string().min(5).max(64);
+
+const createPostSchema = {
+  title: postTitleSchema.required(),
+  content: postContent.required(),
+  image: postImage.required(),
+  author: postAuthor.required(),
+};
+
+const updatePostSchema = {
+  title: postTitleSchema,
+  content: postContent,
+  image: postImage,
+  author: postAuthor,
+};
+
+module.exports = {
+  postIdSchema,
+  createPostSchema,
+  updatePostSchema,
+};
