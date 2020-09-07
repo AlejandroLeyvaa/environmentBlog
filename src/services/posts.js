a322ac793290fd98106473190caf603f4b26bc64
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2020-09-07 17:37:16 -0600

    UpdatePost and deletPost added in services/posts.js

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,21 +1,40 @@
-const MongoLib = require('../lib/mongo');
+const MongoLib = require("../lib/mongo");
 
 class PostsService {
   constructor() {
-    this.collection = 'posts';
+    this.collection = "posts";
     this.mongoDB = new MongoLib();
-  };
+  }
 
-  async getposts({ title }) {
-    const query = title && { title: { $in: title } };
+  async getPosts({ tags }) {
+    const query = tags && { tags: { $in: tags } };
     const posts = await this.mongoDB.getAll(this.collection, query);
     return posts || [];
-  };
+  }
+
+  async getPost({ postId }) {
+    const post = await this.mongoDB.get(this.collection, postId);
+    return post || {};
+  }
 
   async createPost({ post }) {
     const createPostId = await this.mongoDB.create(this.collection, post);
     return createPostId;
-  };
-};
+  }
+
+  async updatePost({ postId, post } = {}) {
+    const updatedPostId = await this.mongoDB.update(
+      this.collection,
+      postId,
+      post
+    );
+    return updatedPostId;
+  }
+
+  async deletePost({ postId }) {
+    const deletedPostId = await this.mongoDB.delete(this.collection, postId);
+    return deletedPostId;
+  }
+}
 
 module.exports = PostsService;
